4a4a9ef8843865ac451eb62c1d9011f46a918475
Jennifer <jennifer_van_@hotmail.com>
2019-04-02 18:06:57 -0700

    added nav scss/ footer scss/lessons page scss

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,59 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Menu
+
+const body = document.body;
+const hamburger = document.querySelector('.hamburger-menu');
+const bar = document.querySelector('.bar');
+
+hamburger.addEventListener('click', showMenu);
+
+function showMenu(){
+    body.classList.toggle('show');
+    bar.classList.toggle('animate');
+}
+
+
+function openMenu(){
+  body.classList.toggle('show');
+nav.classList.add('activated');
+}
+
+// Media Query Event Listener
+// - This is used to remove the "activated"
+// class from the navigation when the user
+// resizes the browser
+
+// Create a media query list using
+// matchMedia
+const mql = window.matchMedia('(min-width: 560px)');
+
+// Add a Media Query Listener to the 
+// above media query list
+mql.addListener(removeTransition);
+
+// Function to remove the transition
+// from the navigation when the user
+// resizes the browser to desktop
+// sizes. In this case when the
+// screen width becomes wider then
+// 560px
+function removeTransition(e){
+// e -> is the event object
+// e.matches -> stores a true false
+// value depending if the media query
+// list set above matches or not
+if(e.matches){
+  body.classList.remove('show');
+  nav.classList.remove('activated');
+}
+}
+
+menu.addEventListener('mousedown', function(e){
+
+  e.preventDefault();
+
+});
+
